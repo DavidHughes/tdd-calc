e5ee5adf3bda41f7b1bf8fcef0a098408a803899
David Hughes <david.hughes125@gmail.com>
2014-07-07 15:18:22 +0100

    Remove "test.js" from watched files

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
     },
     watch: {
       scripts: {
-        files: [jsScripts, 'test.js'],
+        files: [jsScripts],
         tasks: ['jshint', 'simplemocha']
       }
     },
